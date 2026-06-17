ebba9cd5f383e2dcc1af11c86fb5505d10c467a9
forworks286-star <forworks286@gmail.com>
2026-06-17 14:36:53 +0100

    Create security.py

---
+++ app/core/security.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from .config import settings
+
+def create_token(data: dict):
+    expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    data.update({"exp": expire})
+    return jwt.encode(data, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+def decode_token(token: str):
+    try:
+        return jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+    except Exception:
+        return None
