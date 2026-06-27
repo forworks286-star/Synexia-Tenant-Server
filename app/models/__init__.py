af663881dd86b90ebf1d6db42e6fbb577809d211
forworks286-star <forworks286@gmail.com>
2026-06-27 15:47:59 +0100

    c c

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,8 @@
-
+from .users import User
+from .produits import Produit, Lot, Fournisseur
+from .mouvements import Mouvement
+from .factures import Facture
+from .alertes import Alerte, CommandeAuto
+from .integrations import CameraEvent, EnergieLog
+from .audit import AuditLog
+from .tenant_config import TenantConfig
